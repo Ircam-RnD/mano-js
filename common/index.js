9aa08c21df55f63ebb289532172913675a38a5c9
Joseph <joseph.larralde@gmail.com>
2017-08-31 19:13:24 +0200

    minor fix

--- common/index.js
+++ common/index.js
@@ -6,12 +6,6 @@ Object.defineProperty(exports, "__esModule", {
 
 var _translators = require('./translators');
 
-Object.defineProperty(exports, 'rapidMixDocVersion', {
-  enumerable: true,
-  get: function get() {
-    return _translators.rapidMixDocVersion;
-  }
-});
 Object.defineProperty(exports, 'rapidMixToXmmTrainingSet', {
   enumerable: true,
   get: function get() {
@@ -24,4 +18,25 @@ Object.defineProperty(exports, 'xmmToRapidMixTrainingSet', {
     return _translators.xmmToRapidMixTrainingSet;
   }
 });
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInZhbGlkYXRvcnMuanMiXSwibmFtZXMiOlsicmFwaWRNaXhEb2NWZXJzaW9uIiwicmFwaWRNaXhUb1htbVRyYWluaW5nU2V0IiwieG1tVG9SYXBpZE1peFRyYWluaW5nU2V0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozt3QkFDRUEsa0I7Ozs7Ozt3QkFDQUMsd0I7Ozs7Ozt3QkFDQUMsd0IiLCJmaWxlIjoidmFsaWRhdG9ycy5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCB7XG4gIHJhcGlkTWl4RG9jVmVyc2lvbixcbiAgcmFwaWRNaXhUb1htbVRyYWluaW5nU2V0LFxuICB4bW1Ub1JhcGlkTWl4VHJhaW5pbmdTZXRcbn0gZnJvbSAnLi90cmFuc2xhdG9ycyc7Il19
+Object.defineProperty(exports, 'xmmToRapidMixModel', {
+  enumerable: true,
+  get: function get() {
+    return _translators.xmmToRapidMixModel;
+  }
+});
+Object.defineProperty(exports, 'rapidMixToXmmModel', {
+  enumerable: true,
+  get: function get() {
+    return _translators.rapidMixToXmmModel;
+  }
+});
+
+var _constants = require('./constants');
+
+Object.defineProperty(exports, 'rapidMixDocVersion', {
+  enumerable: true,
+  get: function get() {
+    return _constants.rapidMixDocVersion;
+  }
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInZhbGlkYXRvcnMuanMiXSwibmFtZXMiOlsicmFwaWRNaXhUb1htbVRyYWluaW5nU2V0IiwieG1tVG9SYXBpZE1peFRyYWluaW5nU2V0IiwieG1tVG9SYXBpZE1peE1vZGVsIiwicmFwaWRNaXhUb1htbU1vZGVsIiwicmFwaWRNaXhEb2NWZXJzaW9uIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozt3QkFDRUEsd0I7Ozs7Ozt3QkFDQUMsd0I7Ozs7Ozt3QkFDQUMsa0I7Ozs7Ozt3QkFDQUMsa0I7Ozs7Ozs7OztzQkFHT0Msa0IiLCJmaWxlIjoidmFsaWRhdG9ycy5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCB7XG4gIHJhcGlkTWl4VG9YbW1UcmFpbmluZ1NldCxcbiAgeG1tVG9SYXBpZE1peFRyYWluaW5nU2V0LFxuICB4bW1Ub1JhcGlkTWl4TW9kZWwsXG4gIHJhcGlkTWl4VG9YbW1Nb2RlbCxcbn0gZnJvbSAnLi90cmFuc2xhdG9ycyc7XG5cbmV4cG9ydCB7IHJhcGlkTWl4RG9jVmVyc2lvbiB9IGZyb20gJy4vY29uc3RhbnRzJzsiXX0=
