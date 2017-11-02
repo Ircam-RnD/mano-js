4a06468274864dd96fee36a6570a99f97fe6a122
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-11-02 15:29:08 +0100

    fixed path

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,4 @@
-export { default as Example } from './Example';
-export { default as TrainingData } from './TrainingData';
+export { default as Example } from '../common/Example';
+export { default as TrainingData } from '../common/TrainingData';
 export { default as ProcessedSensors } from './ProcessedSensors';
 export { default as XmmProcessor } from '../common/XmmProcessor';
